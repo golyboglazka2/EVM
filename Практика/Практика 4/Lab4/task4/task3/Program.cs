daf1067c1bd054e7f6b01c5c9e3001be59275113
golyboglazka2 <99270577+golyboglazka2@users.noreply.github.com>
2023-12-07 12:12:43 +0500

    g

--- Практика/Практика 4/Lab4/task4/task3/Program.cs
+++ Практика/Практика 4/Lab4/task4/task3/Program.cs
@@ -1,40 +1,12 @@
-﻿using Raven.Iot.Device;
-using Raven.Iot.Device.GpioExpander;
+﻿using System;
+using System.Threading;
 using System.Device.I2c;
-using System.Device.Gpio;
-using UnitsNet;
-using Iot.Device.OneWire;
+using Iot.Device;
 using Iot.Device.Adc;
+using Raven.Iot.Device;
+using Raven.Iot.Device.GpioExpander;
+using Raven.Iot.Device.Ina219;
+using UnitsNet;
 using Spire.Xls;
-using Iot.Device.RotaryEncoder;
-
-//////////////////////////////////task 4      0.6A 5B  0.1Om
-if (DeviceHelper.GetIna219Devices() is [I2cConnectionSettings settings])
-{
-    //Создание объекта Workbook 
-    Workbook workbook = new Workbook();
-
-    //Получение первой рабочей страницы
-    Worksheet worksheet = workbook.Worksheets[0];
-
-    worksheet.Range[1, 1].Value = "mA";
-    worksheet.Range[1, 2].Value = "mV";
-    worksheet.Range[1, 3].Value = "mW";
-
-    using Ina219 ina = new Ina219(settings);
-
-
-        double I = ina.ReadCurrent().Milliamperes;
-        double V = ina.ReadBusVoltage().Millivolts;
-        double P = (double)ina.ReadPower().Milliwatts;
-
-        worksheet.Range[indexer + 2, 1].Value = I.ToString();
-        worksheet.Range[indexer + 2, 2].Value = V.ToString();
-        worksheet.Range[indexer + 2, 3].Value = P.ToString();
-
-        Console.WriteLine($"mesurment {indexer+1}: I = {I}mA, V = {V}mV, P = {P}mW");
-    
-    workbook.SaveToFile("WriteToCells.xlsx");
-}
-
+using System.Device.Gpio;
 
